e7b7dae021867bfaf1c68dc6a62f7c7cb33c51c0
Joe-Kass <jrkass@bu.edu>
2022-01-22 17:51:37 -0500

    Base classes added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Traffic-Sim
+This is a project for Object Software Design (CISC 6375) Spring 2022
--- Vehicle.py
+++ Vehicle.py
@@ -4,11 +4,32 @@ Wed Jan 19 2022
 Joe Kass
 """
 
-class Vehicle:
-    pass
+class Traffic_Object:
+    """Base class for simulation of traffic"""
+    def __init__(self, speed, direction, location):
+        self.speed = speed
+        self.direction = direction
+        self.location = location
+    
+    def accelerate(self, to_speed):
+        pass
+    
+    def decelerate(self, to_speed):
+        pass
+    
+    def turn(self, direction, degrees):
+        pass
 
-class Car(Vehicle):
-    pass
+class Car(Traffic_Object):
+    """Standard class for small motor vehicle"""
+    def __init__(self, speed, direction, location):
+        super().__init__(speed, direction, location)
 
-class Truck(Vehicle):
-    pass
+class Truck(Traffic_Object):
+    """Standard class for large motor vehicle"""
+    def __init__(self, speed, direction, location, load_weight):
+        super().__init__(speed, direction, location)
+        self.load_weight = load_weight
+        
+    def set_load_weight(self, weight):
+        self.load_weight = weight
