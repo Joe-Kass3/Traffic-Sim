ff46cce3277d2716eb6d1edf7f7d8fcc7519f3fd
Joe-Kass <jrkass@bu.edu>
2022-02-06 16:51:42 -0500

    Added ABS + methods to Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -4,32 +4,112 @@ Wed Jan 19 2022
 Joe Kass
 """
 
-class Vehicle:
+from abc import ABC, abstractmethod
+
+# Define Constants
+ACC_RATE = 3.5              # Acceleration rate for cars in m/s
+ACC_RATE_EMPTY = 2.5        # Acceleration rate for light trucks in m/s
+ACC_RATE_FULL = 1.0         # Acceleration rate for heavy trucks in m/s
+DEC_RATE = 7.0              # Braking rate for cars in m/s
+DEC_RATE_EMPTY = 5.0        # Braking rate for light trucks in m/s
+DEC_RATE_FULL = 2.0         # Braking rate for light trucks in m/s
+MPS_TO_MPH = 2.237
+
+class Vehicle(ABC):
     """Base class for simulation of traffic"""
-    def __init__(self, speed, direction, location):
+    def __init__(self, speed, desired_speed, direction, location):
         self.speed = speed
+        self.desired_speed = desired_speed
         self.direction = direction
         self.location = location
     
-    def accelerate(self, to_speed):
-        pass
+    @abstractmethod
+    def accelerate(self, seconds):
+        ...
     
-    def decelerate(self, to_speed):
-        pass
+    @abstractmethod
+    def decelerate(self, seconds):
+        ...
     
+    @abstractmethod
     def turn(self, direction, degrees):
-        pass
+        ...
+
+    def update_speed(self, seconds):
+        if (self.speed > self.desired_speed):
+            self.decelerate(seconds)
+        elif (self.speed < self.desired_speed):
+            self.accelerate(seconds)
+
 
 class Car(Vehicle):
     """Standard class for small motor vehicle"""
-    def __init__(self, speed, direction, location):
-        super().__init__(speed, direction, location)
+    def __init__(self, speed, desired_speed, direction, location):
+        super().__init__(speed, desired_speed, direction, location)
+    
+    def accelerate(self, seconds):
+        temp_speed = self.speed + (ACC_RATE * seconds * MPS_TO_MPH)
+        if (temp_speed > self.desired_speed):
+            self.speed = self.desired_speed
+        else:
+            self.speed = temp_speed
+
+    def decelerate(self, seconds):
+        temp_speed = self.speed - (DEC_RATE * seconds * MPS_TO_MPH)
+        if (temp_speed < self.desired_speed):
+            self.speed = self.desired_speed
+        else:
+            self.speed = temp_speed
+    
+
+    def turn(self, direction, degrees):
+        pass
+
 
 class Truck(Vehicle):
     """Standard class for large motor vehicle"""
-    def __init__(self, speed, direction, location, load_weight):
-        super().__init__(speed, direction, location)
+    def __init__(self, speed, desired_speed, direction, location, load_weight):
+        super().__init__(speed, desired_speed, direction, location)
         self.load_weight = load_weight
+    
+# Disabled for timebeing, will remove entirely unless there is some need for control of setting load weight
+#    def set_load_weight(self, weight):
+#        self.load_weight = weight
+    
+    def accelerate(self, seconds):
+        if (self.load_weight <= 5):
+            temp_speed = self.speed + (ACC_RATE_EMPTY * seconds * MPS_TO_MPH)
+        else:
+            temp_speed = self.speed + (ACC_RATE_FULL * seconds * MPS_TO_MPH)
+            
+        if (temp_speed > self.desired_speed):
+            self.speed = self.desired_speed
+        else:
+            self.speed = temp_speed
+
+                
+    def decelerate(self, seconds):
+        if (self.load_weight <= 5):
+            temp_speed = self.speed - (DEC_RATE_EMPTY * seconds * MPS_TO_MPH)
+        else:
+            temp_speed = self.speed - (DEC_RATE_FULL * seconds * MPS_TO_MPH)
+            
+        if (temp_speed < self.desired_speed):
+            self.speed = self.desired_speed
+        else:
+            self.speed = temp_speed
+    
+    def turn(self, direction, degrees):
+        pass
+    
+    
+if __name__ == "__main__":
+    car = Car(0, 65, 0, 0)
+    truck1 = Truck(0, 55, 0, 1, 4)
+    truck2 = Truck(0, 50, 0, 2, 8)
+    v_list = [car, truck1, truck2]
+    for i in range(11):
+        for j in range(len(v_list)):
+            v_list[j].update_speed(1)
+            print('{0} speed: {1:8.2f} mph'.format(type(v_list[j]).__name__, v_list[j].speed))
         
-    def set_load_weight(self, weight):
-        self.load_weight = weight
